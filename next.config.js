7d67c8cedf3cdaf52d5e02ad50935e62f1f3c476
zhixin <liuzhixin.sg@gmail.com>
2025-04-17 17:43:17 +0800

    change basepath

--- next.config.js
+++ next.config.js
@@ -1,23 +1,15 @@
-/**
- * @type {import('next').NextConfig}
- * 
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
-    
-    formats:['image/webp']
+    unoptimized: true,
+    formats: ['image/webp']
   },
   optimizeFonts: false,
-  basePath: ''
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  basePath: '/valentines',
+  assetPrefix: '/valentines/',
+  trailingSlash: true
+  // Optional: Change links `/me` -> `/me/`
 }
- 
+
 module.exports = nextConfig

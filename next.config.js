c4b726689bc04ce2a746fada5d6de786894bfffc
zhixin <liuzhixin.sg@gmail.com>
2025-04-17 18:36:55 +0800

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     unoptimized: true // Important for GitHub Pages compatibility
   },
   optimizeFonts: false, // Optional optimization (can be true or false)
-  basePath: '/valentines', // Your site's path on GitHub Pages
-  assetPrefix: '/valentines/', // Matches basePath for assets
+  basePath: '', // Your site's path on GitHub Pages
+  assetPrefix: '', // Matches basePath for assets
   trailingSlash: true // Recommended for consistent routing
 };
 

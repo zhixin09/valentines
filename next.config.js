2faca24a7111180e49d04aed5523b60aa38d708c
zhixin <liuzhixin.sg@gmail.com>
2025-04-17 17:40:33 +0800

    change basepath

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     formats:['image/webp']
   },
   optimizeFonts: false,
-  basePath: '/valentines'
+  basePath: '/me.index.html'
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
  
